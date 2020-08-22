4285732dbe61f2d0adb955d7ba8d781ff03412de
wloodar <wlodardev@gmail.com>
2020-08-22 04:38:00 +0200

    First done version of events

--- src/components/Pages/Events/Events.js
+++ src/components/Pages/Events/Events.js
@@ -3,31 +3,74 @@ import { withRouter, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import axios from 'axios';
+import moment from 'moment';
+import 'moment/locale/pl';
+import { createUrl } from '../../../functions/ImageUrl';
 import s from './Events.module.scss';
 
 import EventCover from './example.jpg';
+import EventGraphic from './event.svg';
 
 class Events extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            events: []
+            events: [],
+            loading: true
         };
     }
 
     componentDidMount() {
         axios.get(`${process.env.REACT_APP_GLOBAL_API_URL}/events/fetch/all`).then(res => {
             if (res.data.status === 1) {
-                this.setState({ events: res.data.events });
+                this.setState({ loading: false, events: res.data.events });
             }
         });
     }
 
     render() {
+        const { events, loading } = this.state;
+
         return (
             <main className={s.main}>
-                
+                {loading ? <p>laduje ...</p> : null}
+                {!loading && events.length === 0 ? <div className={s.empty}>
+                    <img src={EventGraphic}/>
+                    <h5>Na ten moment nie odbywają się żadne wydarzenia...</h5>
+                </div> : null}
+                {!loading && events.length > 0 ? <div className={s.events}>
+                    <div className={s.header}>
+                        <h2>Zbliżające się wydarzenia</h2>
+                    </div>
+                    <ul>
+                        {events.map((val, key) => <li key={key}>
+                            <div className={s.banner}>
+                                <div className={s.calendar}>
+                                    <div className={s.calendar__top}></div>
+                                    <div className={s.calendar__day}>
+                                        <p>{moment.unix(val.time).format("DD")}</p>
+                                    </div>
+                                </div>
+                                <div className={s.cover}>
+                                    <img src={createUrl(val.cover_pic, "_medium")}/>
+                                </div>
+                            </div>
+                            <div className={s.time}>
+                                <p>{moment.unix(val.time).locale('pl').format("DD MMMM YYYY o HH:mm")}</p>
+                            </div>
+                            <div className={s.name}>
+                                <h3>{val.name}</h3>
+                            </div>
+                            <div className={s.place}>
+                                <p>{val.place}</p>
+                            </div>
+                            <div className={s.description}>
+                                <p>{val.description}</p>
+                            </div>
+                        </li>)}
+                    </ul>
+                </div> : null}
             </main>
         )
     }
